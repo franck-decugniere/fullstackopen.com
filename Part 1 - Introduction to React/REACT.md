a191939f06d6a499d38a142c6673c9582ad8aedb
Franck <franck.decugniere@gmail.com>
2024-02-21 22:16:46 +0100

    refacto

--- Part 1 - Introduction to React/REACT.md
+++ Part 1 - Introduction to React/REACT.md
@@ -50,13 +50,25 @@ const App = () => {
 ```
 
 ## Complex state
-> [!WARNING]  
+> [!WARNING]  STATE IMMUTABILITY
 > When setting state (`Object`,`Arrays`,..), a **new value** must be passed (no render if oldState === newState)
+>
+> > What is IMMUTABILITY ?
+> > - Make a copy of original
+> > - Make change to copy
+> > - Replace original with copy
+>
+
+> [!WARNING] UPDATE OF STATE IS ASYNCHRONOUS  
+> `setTotal` will not have the correct `left` value
 
 ```js
   const [clicks, setClicks] = useState({
     left: 0, right: 0
   })
-  const handleLeftClick = () => setClicks({ ...clicks, left: clicks.left + 1 })
+  const handleLeftClick = () => {
+    setClicks({ ...clicks, left: clicks.left + 1 })
+    setTotal(click.left + click.right)
+  }
 ```
 
--- Part 1 - Introduction to React/lesson/src/AppD.jsx
+++ Part 1 - Introduction to React/lesson/src/AppD.jsx
@@ -3,21 +3,51 @@ import { useState } from 'react';
 
 const AppD = () => {
   const [clicks, setClicks] = useState({ left: 0, right: 0 });
-  const handleLeftClick = () => setClicks({ ...clicks, left: clicks.left + 1 })
-  const handleRightClick = () => setClicks({ ...clicks, right: clicks.right + 1 })
-  
-    return (
+  const [allClicks, setAll] = useState([]);
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setClicks({ ...clicks, left: clicks.left + 1 })
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setClicks({ ...clicks, right: clicks.right + 1 })
+  }
+
+  return (
+      <>
       <div>
         {clicks.left}
-        <button onClick={handleLeftClick}>
-          left
-        </button>
-        <button onClick={handleRightClick}>
-          right
-        </button>
+        <Button handleClick={handleLeftClick} text='Left'/>
+        <Button handleClick={handleRightClick} text='Right'/>
         {clicks.right}
       </div>
+      <History allClicks={ allClicks} />
+      </>
     )
+}
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
 }
   
 export default AppD
