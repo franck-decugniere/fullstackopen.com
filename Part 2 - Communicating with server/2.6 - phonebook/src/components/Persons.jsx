fc904fe1a06da9944c1b01abb15b4891931d56e1
FDE <franck.decugniere@gmail.com>
2024-02-26 11:44:38 +0100

    rename phonebook folder

--- Part 2 - Communicating with server/2.6 - phonebook/src/components/Persons.jsx
+++ Part 2 - Communicating with server/2.6 - phonebook/src/components/Persons.jsx
@@ -1,6 +1,5 @@
 const Persons = ({ persons, newFilter }) => {
-    return (
-        <ul>
+    return (<ul>
             {persons
                 .filter((person) => newFilter.length == 0 || person.name.toUpperCase().includes(newFilter.toUpperCase()))
                 .map((person) => { return <li key={person.name}>{person.name} {person.number}</li> })}
