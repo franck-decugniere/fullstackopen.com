5bdaa325e81206151ddf0c86c16ae6b001f11d90
FDE <franck.decugniere@gmail.com>
2024-02-26 22:10:10 +0100

    ex 2.11 - fetch phonebook from server using effect hook & axios

--- Part 2 - Communicating with server/2.6 - phonebook/src/App.jsx
+++ Part 2 - Communicating with server/2.6 - phonebook/src/App.jsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 import { Filter } from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
@@ -23,10 +20,19 @@ const App = () => {
     setNewNumber('')
   }
 
+  const fetchPhoneBook = () => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then( response => setPersons(response.data) )
+  }
+  useEffect(fetchPhoneBook,[])
+
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <Filter value={newFilter} updateFilter={() => setNewFilter(event.target.value)}/>
+
+      {persons.length > 0 && <Filter value={newFilter} updateFilter={() => setNewFilter(event.target.value)}/>}
       
       <h2>Add a new</h2>
       <PersonForm 
@@ -36,7 +42,7 @@ const App = () => {
         handleNewNumber={() => setNewNumber(event.target.value)} 
         handleSubmit={handleSubmit}/>
 
-      <h2>Numbers</h2>
+      {persons.length > 0  && <h2>Numbers</h2>}
       <Persons newFilter={newFilter} persons={persons}/>
       
       <hr />
