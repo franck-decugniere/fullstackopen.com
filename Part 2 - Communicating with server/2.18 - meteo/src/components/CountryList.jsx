23f67f33553fe093da1aba56433765ed6fc2293c
Franck <franck.decugniere@gmail.com>
2024-03-01 11:32:17 +0100

    ex 2.19 - meteo - show button, useEffect to reset state when props change

--- Part 2 - Communicating with server/2.18 - meteo/src/components/CountryList.jsx
+++ Part 2 - Communicating with server/2.18 - meteo/src/components/CountryList.jsx
@@ -1,31 +1,29 @@
+import { useState, useEffect } from "react"
+
 const CountryList = ({ allCountries, maxItems }) => {
     const nbItem = allCountries.length
     const tooManyItems = nbItem > maxItems
+    const [showCountry, setShowCountry] = useState(null)
+
+    useEffect(() => setShowCountry(null), [allCountries])
+    
+    console.log("CountryList", allCountries)
+    if (showCountry) {
+        console.log("Show country", showCountry)
+        return (<Country country={showCountry}/>)
+    }
 
     if (nbItem == 1) {
-        const country = allCountries[0]
-        let languages = '';
-        for (let value of Object.values(country.languages)) {
-            languages += value
-        }
-        console.log("l", languages)
-        return (<>
-            <h1>{country.name.common}</h1>
-            <p>Capital {country.capital}</p>
-            <p>Area {country.area}</p>
-            <h2>Languages:</h2>
-            <ul>
-                {Object.values(country.languages).map(language => {
-                    return <li key={language}>{language}</li>
-                })}
-            </ul>
-            <img src={ country.flags.png} />
-        </>
-        )
+        console.log(allCountries[0])
+         return (<Country country={allCountries[0]}/>)
     }
 
     if (tooManyItems) {
         return (<div>Too many matches, specify another filter</div>)
+    }
+
+    const showChildren = (country) => {
+        setShowCountry(country)
     }
 
     return (
@@ -33,10 +31,27 @@ const CountryList = ({ allCountries, maxItems }) => {
             {
                 allCountries.map(item => {
                     // console.log("item", item)
-                    return <div key={item.name.ccn3}>{item.name.common}</div>
+                    return <div key={item.name.ccn3}>{item.name.common}<button onClick={() => showChildren(item)}>Show</button></div>
                 })
             }
         </>
+    )
+}
+
+const Country = ({ country }) => {
+    console.log("Country", country)
+    return (<>
+        <h1>{country.name.common}</h1>
+        <p>Capital {country.capital}</p>
+        <p>Area {country.area}</p>
+        <h2>Languages:</h2>
+        <ul>
+            {Object.values(country.languages).map(language => {
+                return <li key={language}>{language}</li>
+            })}
+        </ul>
+        <img src={country.flags.png} />
+    </>
     )
 }
 
