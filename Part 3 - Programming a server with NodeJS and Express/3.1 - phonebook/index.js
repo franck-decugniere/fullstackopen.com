5bcf4784ac25070aa88e703b9a94b82ed8f0194c
FDE <franck.decugniere@gmail.com>
2024-03-03 21:53:09 +0100

    ex 3.1 - phonebook - middleware

---
+++ Part 3 - Programming a server with NodeJS and Express/3.1 - phonebook/index.js
@@ -0,0 +1,99 @@
+const express = require("express");
+const app = express();
+app.use(express.json()); // json middleware that parse raw json into js object & assign it to request.body
+
+// Create custom middleware
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+
+app.use(requestLogger);
+
+let phonebook = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/info", (request, response) => {
+  let responseHtml = `<p>Phonebook has info for ${phonebook.length} people</p>`;
+  responseHtml += new Date();
+  response.send(responseHtml);
+});
+
+app.get("/api/persons", (request, response) => {
+  response.json(phonebook);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = phonebook.find((person) => person.id === id);
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  phonebook = phonebook.filter((person) => person.id !== id);
+  response.status(204).end();
+});
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+  if (phonebook.filter((p) => p.name === body.name).length > 0) {
+    return response.status(400).json({
+      error: "name must be unique",
+    });
+  }
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  };
+  phonebook.push(person);
+  response.json(person);
+});
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const generateId = () => {
+  return Math.round(Math.random() * 1000);
+};
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
