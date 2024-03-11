a175662c9d978feecdc6fc945980058af1e9ddd4
Franck <franck.decugniere@gmail.com>
2024-03-11 11:20:44 +0100

    ex 4.2 - Refacto

---
+++ Part 4 - Testing Express servers/4.1 - blog/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const blogRouter = require('./controllers/blog')
+
+mongoose.connect(config.MONGODB_URI_BLOG)
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs', blogRouter)
+
+module.exports = app
---
+++ Part 4 - Testing Express servers/4.1 - blog/utils/config.js
@@ -0,0 +1,6 @@
+const PORT = process.env.PORT
+const MONGODB_URI_BLOG = process.env.MONGODB_URI_BLOG
+
+module.exports = {
+  PORT, MONGODB_URI_BLOG
+}
